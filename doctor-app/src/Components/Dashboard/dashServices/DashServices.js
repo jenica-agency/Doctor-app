f456db86c9caa1dfe74f0b96a26810fe4a0b0fe7
Mohamed 3bdelrahman <mohamedbdelrahman2@gmail.com>
2023-03-22 18:16:39 +0200

    create dashboard

---
+++ doctor-app/src/Components/Dashboard/dashServices/DashServices.js
@@ -0,0 +1,12 @@
+const DashServices = () => {
+  return (
+    <div className="services">
+      <h2>Hi from Services</h2>
+      <h2>Hi from Services</h2>
+      <h2>Hi from Services</h2>
+      <h2>Hi from Services</h2>
+    </div>
+  );
+};
+
+export default DashServices;
---
+++ doctor-app/src/Components/Dashboard/main/Main.js
@@ -0,0 +1,10 @@
+const Main = () => {
+  return (
+    <div className="main">
+      <h1>hiiiii</h1>
+      <h1>Navvvvvvvv</h1>
+    </div>
+  );
+};
+
+export default Main;
