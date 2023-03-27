5dfe818ba81663d64c7711f6b1473a9ca4668d94
hagar00 <hagarhassan813@gmail.com>
2023-03-27 16:38:46 +0200

    add type write

--- doctor-app/src/Components/Home/Home.js
+++ doctor-app/src/Components/Home/Home.js
@@ -31,6 +31,8 @@ import {useTranslation} from 'react-i18next';
 // page 
 import BlogsFun from '../Blogs/BlogsFun';
 
+// type write 
+import TypeWriterEffect from 'react-typewriter-effect';
 
 // data
 import dataBlog from '../Api/dataBlogs';
@@ -67,7 +69,25 @@ function Home() {
                   </div>
                   <div className='cover-right col-lg-6 col-md-6 col-sm-12  content-home'>
                     <h2 className='title-cover-home'>{t('home.TitleCoverHome')}</h2>
-                    <p className='p-cover-home'> {t('home.ContentHomeParagraph')}</p>
+                    {/* <p className='p-cover-home'> {t('home.ContentHomeParagraph')}</p> */}
+
+                    <TypeWriterEffect
+                        textStyle={{
+                          fontFamily: 'Red Hat Display',
+                          color: 'var(--light-color)',
+                          fontWeight: 500,
+                          fontSize: '1.5em',
+                          direction: 'rtl'
+                        }}
+                        startDelay={100}
+                        cursorColor="var(--light-color)"
+                        multiText={[
+                          t('home.ContentHomeParagraph'),
+                          t('home.ContentHomeParagraph'),
+                        ]}
+                        multiTextDelay={1000}
+                        typeSpeed={100}
+                      />
                     <h3 className='pt-3 doctor-name-cover-home'> {t('home.DoctorNameCoverHome')}</h3>
                   </div>
               </div>
