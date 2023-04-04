33843c0629fb53b7ecbc7921fa96344ae60587e3
Mohamed 3bdelrahman <mohamedbdelrahman2@gmail.com>
2023-04-04 13:25:36 +0200

    edit reservation model

--- backend/models/reserve.js
+++ backend/models/reserve.js
@@ -5,13 +5,13 @@ const Schema = mongoose.Schema;
 const reserveSchema = new Schema(
   {
     user_name: { type: String, required: true },
-    email: { type: String, required: true },
     phone_num: { type: String, required: true },
     other_phone: { type: String },
     state: { type: Boolean, default: false },
     reserve_date: { type: Date, required: true },
+    message: { type: String },
   },
   { timestamps: true }
 );
 
-module.exports = mongoose.model("reserve", reserveSchema);
+module.exports = mongoose.model("reservation", reserveSchema);
